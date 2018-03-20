093ad4b3d686f4a9ee5c102ad0b0cc66c8788c64
Cyrusiak <m.adamkiewicz95@gmail.com>
2018-03-20 12:30:35 +0100

    Add project files.

---
+++ Basic_Objectivity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Objectivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dupa");
+            
+        }
+    }
+}
